b66e76c5b5ad6271d510673b37904a59c8fec391
André Luiz Jaques Alves <102561368+andreluizjaques@users.noreply.github.com>
2022-06-06 20:14:32 -0300

    Update 10128.sql

--- codigos/10128.sql
+++ codigos/10128.sql
@@ -1,2 +1,18 @@
+/*
+Count the number of movies that Abigail Breslin nominated for oscar
+
+Difficulty: Easy
+
+Count the number of movies that Abigail Breslin was nominated for an oscar.
+
+Table:
+year: int
+category: varchar
+nominee: varchar
+movie: varchar
+winner: bool
+id: int
+
+*/
 select count(id) from oscar_nominees
 where nominee = 'Abigail Breslin'
