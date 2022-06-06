260cbc0121f5066ad8ce7805950a7b44688f25ce
André Luiz Jaques Alves <102561368+andreluizjaques@users.noreply.github.com>
2022-06-06 20:17:45 -0300

    Update 10352.sql

--- codigos/10352.sql
+++ codigos/10352.sql
@@ -1,3 +1,18 @@
+/*
+Users By Average Session Time
+
+Difficulty: Medium
+
+Calculate each user's average session time. A session is defined as the time difference between a page_load and page_exit. 
+For simplicity, assume a user has only 1 session per day and if there are multiple of the same events on that day, 
+consider only the latest page_load and earliest page_exit. Output the user_id and their average session time.
+
+Table:
+user_id: int
+timestamp: datetime
+action: varchar
+*/
+
 select user_id, avg(session_time) as average_session_time 
     from(
         select user_id, d, timestampdiff(second,latest_page_load, eartliest_page_exit) as session_time 
