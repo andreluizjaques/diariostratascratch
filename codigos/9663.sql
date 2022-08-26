23be42f3770cbd34bcf6fb940be0e18ef35af65a
André Luiz Jaques Alves <102561368+andreluizjaques@users.noreply.github.com>
2022-08-26 19:44:22 -0300

    Update 9663.sql

--- codigos/9663.sql
+++ codigos/9663.sql
@@ -4,28 +4,17 @@ Difficulty: Easy
 
 Find the most profitable company from the financial sector. Output the result along with the continent.
 
-company:
-varchar
-sector:
-varchar
-industry:
-varchar
-continent:
-varchar
-country:
-varchar
-marketvalue:
-float
-sales:
-float
-profits:
-float
-assets:
-float
-rank:
-int
-forbeswebpage:
-varchar 
+company: varchar
+sector: varchar
+industry: varchar
+continent: varchar
+country: varchar
+marketvalue: float
+sales: float
+profits: float
+assets: float
+rank: int
+forbeswebpage: varchar 
 */
 
 select company, continent from forbes_global_2010_2014
